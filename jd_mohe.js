dfdade0deac261f8f8bb475643064c8c807bac69
yuannian1112 <84368132+yuannian1112@users.noreply.github.com>
2021-08-09 00:39:17 +0800

    Update jd_mohe.js

--- jd_mohe.js
+++ jd_mohe.js
@@ -7,17 +7,17 @@
 =================================Quantumultx=========================
 [task_local]
 #5G超级盲盒
-20 0,1-23/3 * * * https://raw.githubusercontent.com/Aaron-lv/sync/jd_scripts/jd_mohe.js, tag=5G超级盲盒, img-url=https://raw.githubusercontent.com/Orz-3/mini/master/Color/jd.png, enabled=true
+0 0,1-23/3 * * * https://raw.githubusercontent.com/Aaron-lv/sync/jd_scripts/jd_mohe.js, tag=5G超级盲盒, img-url=https://raw.githubusercontent.com/Orz-3/mini/master/Color/jd.png, enabled=true
 
 =================================Loon===================================
 [Script]
 cron "20 0,1-23/3 * * *" script-path=https://raw.githubusercontent.com/Aaron-lv/sync/jd_scripts/jd_mohe.js,tag=5G超级盲盒
 
 ===================================Surge================================
-5G超级盲盒 = type=cron,cronexp="20 0,1-23/3 * * *",wake-system=1,timeout=3600,script-path=https://raw.githubusercontent.com/Aaron-lv/sync/jd_scripts/jd_mohe.js
+5G超级盲盒 = type=cron,cronexp="0 0,1-23/3 * * *",wake-system=1,timeout=3600,script-path=https://raw.githubusercontent.com/Aaron-lv/sync/jd_scripts/jd_mohe.js
 
 ====================================小火箭=============================
-5G超级盲盒 = type=cron,script-path=https://raw.githubusercontent.com/Aaron-lv/sync/jd_scripts/jd_mohe.js, cronexpr="20 0,1-23/3 * * *", timeout=3600, enable=true
+5G超级盲盒 = type=cron,script-path=https://raw.githubusercontent.com/Aaron-lv/sync/jd_scripts/jd_mohe.js, cronexpr="0 0,1-23/3 * * *", timeout=3600, enable=true
  */
 const $ = new Env('5G超级盲盒');
 const notify = $.isNode() ? require('./sendNotify') : '';
@@ -129,7 +129,7 @@ async function task0() {
 }
 function addShare(shareId) {
   return new Promise((resolve) => {
-    const body = {"shareId":shareId,"apiMapping":"/active/shareInfo"}
+    const body = {"shareId":shareId,"apiMapping":"/active/addShare"}
     $.get(taskurl(body), (err, resp, data) => {
       try {
         if (err) {
@@ -138,15 +138,9 @@ function addShare(shareId) {
         } else {
           data = JSON.parse(data);
           if (data['code'] === 200) {
-            if (data.data.self) {
-              console.log(`\n不能助力自己\n`);
-            } else {
-              if (data.data.hasHelp) {
-                console.log(`\n助力好友【${data.data.nickName}】成功\n`);
-              } else {
-                console.log(`\n助力好友【${data.data.nickName}】失败：好友助力已满或您没有助力次数\n`);
-              }
-            }
+            console.log(`助力好友【${data.data}】成功\n`);
+          } else {
+            console.log(`助力失败：${data.msg}`);
           }
         }
       } catch (e) {
