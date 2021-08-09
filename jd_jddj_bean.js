f71c0c78085554314d94bad14448b2729a7f1b91
yuannian1112 <84368132+yuannian1112@users.noreply.github.com>
2021-08-09 21:43:09 +0800

    Update jd_jddj_bean.js

--- jd_jddj_bean.js
+++ jd_jddj_bean.js
@@ -6,10 +6,10 @@ boxjs订阅地址:https://gitee.com/passerby-b/javascript/raw/master/JD/passerby
 TG群:https://t.me/passerbyb2021
 
 [task_local]
-10 0 * * * https://raw.githubusercontent.com/passerby-b/JDDJ/main/jddj_bean.js
+35 0,6,12 * * * https://raw.githubusercontent.com/passerby-b/JDDJ/main/jddj_bean.js
 
 [Script]
-cron "10 0 * * *" script-path=https://raw.githubusercontent.com/passerby-b/JDDJ/main/jddj_bean.js,tag=京东到家鲜豆任务
+cron "35 0,6,12 * * *" script-path=https://raw.githubusercontent.com/passerby-b/JDDJ/main/jddj_bean.js,tag=京东到家鲜豆任务
 
 */
 
