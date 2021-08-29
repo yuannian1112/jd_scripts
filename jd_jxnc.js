bd24491900794f31fd9da7ba588a23d6c9af378e
yuannian1112 <84368132+yuannian1112@users.noreply.github.com>
2021-08-29 22:32:19 +0800

    Update jd_jxnc.js

--- jd_jxnc.js
+++ jd_jxnc.js
@@ -189,7 +189,7 @@ function requireConfig() {
 
     try {
       let options = {
-        "url": ``,
+        "url": `https://cdn.jsdelivr.net/gh/Aaron-lv/updateTeam@master/shareCodes/jxnc.txt`,
         "headers": {
           "Accept": "application/json,text/plain, */*",
           "Content-Type": "application/x-www-form-urlencoded",
