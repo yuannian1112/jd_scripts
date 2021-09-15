b641eaa8fc2ccda66ae9c4cc62be5e10e7f1b129
yuannian1112 <2669371896@qq.com>
2021-09-15 10:21:35 +0800

    今天没水

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -202,7 +202,7 @@
 #开卡31
 33 3,19 14-18 9 * node /scripts/jd_opencard31.js >> /scripts/logs/jd_opencard31.log 2>&1
 #开卡32
-18 5,20 15-23 9 * node /scripts/jd_opencard32.js >> /scripts/logs/jd_opencard32.log 2>&1
+#18 5,20 15-23 9 * node /scripts/jd_opencard32.js >> /scripts/logs/jd_opencard32.log 2>&1
 #希捷品牌日瓜分百万京豆
 17 2,10 * 9,10 * node /scripts/jd_UnknownTask4.js >> /scripts/logs/jd_UnknownTask4.log 2>&1
 #积分换话费
