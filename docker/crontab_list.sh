260f8333cb2702530cb12034c68c0f18f75a5fe5
yuannian1112 <2669371896@qq.com>
2021-09-16 14:09:10 +0800

    更新

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -231,6 +231,8 @@
 23 10,21 13-26 9 * node /scripts/jd_film_museum.js >> /scripts/logs/jd_film_museum.log 2>&1
 # 企有此礼
 30 0,20 * * * node /scripts/jd_qycl.js >> /scripts/logs/jd_qycl.log 2>&1
+# 京东价格保护
+30 2 * * * node /scripts/jd_priceProtect.js >> /scripts/logs/jd_priceProtect.log 2>&1
 #
 0 6 * * * node /scripts/jd_focus.js >> /scripts/logs/jd_focus.log 2>&1
 #早起签到
