260834d8a99ce483e8e8f930598ddc61254406e6
yuannian1112 <2669371896@qq.com>
2021-09-18 00:32:44 +0800

    新增东东世界

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -231,6 +231,8 @@
 30 0,20 * * * node /scripts/jd_qycl.js >> /scripts/logs/jd_qycl.log 2>&1
 # 京东价格保护
 30 2 * * * node /scripts/jd_priceProtect.js >> /scripts/logs/jd_priceProtect.log 2>&1
+# 东东世界
+15 3,16 * * * node /scripts/jd_ddworld.js >> /scripts/logs/jd_ddworld.log 2>&1
 #
 0 6 * * * node /scripts/jd_focus.js >> /scripts/logs/jd_focus.log 2>&1
 #早起签到
