976e423272b1b6e443bf5644ace1248518e3007e
yuannian1112 <2669371896@qq.com>
2021-09-25 23:25:26 +0800

    新增魔方

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -229,6 +229,8 @@
 20 0,16 * * * node /scripts/jd_ddwj.js >> /scripts/logs/jd_ddwj.log 2>&1
 # 热血心跳,狂解压
 5 6,18 * * * node /scripts/jd_decompression.js >> /scripts/logs/jd_decompression.log 2>&1
+# 魔方
+11 7,19 * * * node /scripts/jd_mofang.js >> /scripts/logs/jd_mofang.log 2>&1
 #早起签到
 1 6,7 * * * python3 /jd/scripts/jd_zqfl.py >> /jd/log/jd_zqfl.log 2>&1
 #
