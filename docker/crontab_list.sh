630b95c64252cd572fb49884915628b3fde62082
yuannian1112 <2669371896@qq.com>
2021-09-30 11:58:02 +0800

    删除

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -185,8 +185,6 @@
 5 0,15 * * * node /scripts/jd_opencard27.js >> /scripts/logs/jd_opencard27.log 2>&1
 #开卡36
 18 2,14 22-30 9 * node /scripts/jd_opencard36.js >> /scripts/logs/jd_opencard36.log 2>&1
-#开卡38
-47 4 23-28 9 * node /scripts/jd_opencard38.js >> /scripts/logs/jd_opencard38.log 2>&1
 #开卡39
 45 2,22 * * * node /scripts/jd_opencard39.js >> /scripts/logs/jd_opencard39.log 2>&1
 #积分换话费
