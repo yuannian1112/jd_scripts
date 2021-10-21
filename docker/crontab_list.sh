6cf081246c26b5ce03f272fab21c8ecc3e6c90f2
yuannian1112 <2669371896@qq.com>
2021-10-21 22:05:09 +0800

    环游记

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -233,6 +233,8 @@
 11 0,9 * * * node /scripts/jd_superMarket_qjd.js >> /scripts/logs/jd_superMarket_qjd.log 2>&1
 # 预售福利机
 5 0,2 * * * node /scripts/jd_ys.js >> /scripts/logs/jd_ys.log 2>&1
+# hyj
+0 * * * * node /scripts/jd_hyj.js >> /scripts/logs/jd_hyj.log 2>&1
 #早起签到
 1 6,7 * * * python3 /jd/scripts/jd_zqfl.py >> /jd/log/jd_zqfl.log 2>&1
 #
