e4bbbe22858f08e9fda2001a649d3f90a21a9ae6
yuannian1112 <2669371896@qq.com>
2021-11-09 18:07:30 +0800

    10.09-11.17 大牌联合 11.11狂欢购

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -193,6 +193,8 @@
 45 2,21 5-12 11 * node /scripts/jd_opencard62.js >> /scripts/logs/jd_opencard62.log 2>&1
 #开卡63
 16 0,21 8-30 11 * node /scripts/jd_opencard63.js >> /scripts/logs/jd_opencard63.log 2>&1
+#开卡64
+9 0,18 9-17 11 * node /scripts/jd_opencard64.js >> /scripts/logs/jd_opencard64.log 2>&1
 #积分换话费
 43 5,17 * * * node /scripts/jd_dwapp.js >> /scripts/logs/jd_dwapp.log 2>&1
 # 领券中心签到
