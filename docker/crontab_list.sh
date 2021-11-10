75b83f92202886093e6668c7020786a9c435a038
yuannian1112 <2669371896@qq.com>
2021-11-11 02:33:10 +0800

    大牌联合 赢大额京豆  现在有水

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -195,6 +195,8 @@
 16 0,21 8-30 11 * node /scripts/jd_opencard63.js >> /scripts/logs/jd_opencard63.log 2>&1
 #开卡64
 9 0,18 9-17 11 * node /scripts/jd_opencard64.js >> /scripts/logs/jd_opencard64.log 2>&1
+#开卡65
+19 4,18 4-11 11 * node /scripts/jd_opencard65.js >> /scripts/logs/jd_opencard65.log 2>&1
 #积分换话费
 43 5,17 * * * node /scripts/jd_dwapp.js >> /scripts/logs/jd_dwapp.log 2>&1
 # 领券中心签到
